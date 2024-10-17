e9b904a648c3b235b219b6563b6fbba8755f0ee6
Luis Antonio Guzman Bucio <luisantonioguzmanbucio@Luiss-MacBook-Air.local>
2024-10-17 17:47:28 -0600

    Added interpreter for AE & WAE

--- src/interp.clj
+++ src/interp.clj
@@ -1,1 +1,75 @@
 ; Continuara...
+
+(ns interp
+  (:require [grammars :as wae]))
+
+(defn interp-AE
+  "Interpreta una expresión AE y devuelve su valor numérico."
+  [exp]
+  (cond
+    ;;Caso 1:i es una expresiopn NumG, devolvemos el número directamente.
+    (instance? wae/NumG exp)
+        
+        (:n exp)
+
+    ;caso 2: si es una expresion AddG, evaluamos las dos subepresiones y las sumamos
+    (instance? wae/AddG exp)
+    (+ (interp-AE (:izq exp)) 
+        
+        (interp-AE (:der exp)))
+
+    ;;caso 3: sii es una expresión SubG, evaluamos las dos subexprsiones y las restaos.
+    (instance? wae/SubG exp)
+    (- (interp-AE (:izq exp)) 
+        
+        (interp-AE (:der exp)))
+
+    ;; Si no es ninguno de loscasos anteriores lanzmos un error
+    :else
+    (throw (IllegalArgumentException. "Expresión AE inválida"))))
+
+
+(defn interp-WAE
+  "Interpreta una expresión WAE en un entorno y devuelve su valor numérico."
+  ([exp]
+   (interp-WAE exp {})) ;;iniciamos con un entorno vacío.
+  ([exp env]
+   (cond
+     ;;caso 1: Si es una expresión NumG, devolvemos el número directamente
+     (instance? wae/NumG exp)
+     (:n exp)
+
+     ;;caso 2: Si es una expresión IdG, buscamos su valor en el entorno
+     (instance? wae/IdG exp)
+     (if-let [val (get env (:i exp))]
+       val
+       (throw (IllegalArgumentException. 
+            
+            (str "Variable no definida: " (:i exp)))))
+
+     ;;;caso 3: Si es una expresión AddG, evaluamos las dos subexpresiones y las sumamos
+     (instance? wae/AddG exp)
+        (+ (interp-WAE (:izq exp) env) 
+            
+            (interp-WAE (:der exp) env))
+
+     ;;caso 4: Si es una expresión SubG, evaluamos las dos subexpresiones y las restamos.
+     (instance? wae/SubG exp)
+        (- (interp-WAE (:izq exp) env) 
+            (interp-WAE (:der exp) env))
+
+     ;;caso 5: Si es una expresión WithG, evaluamos la asignación y extendemos el entorno
+     (instance? wae/WithG exp)
+        (let [id (:id (:assign exp))  ;;tomamos el identificador
+            
+            val (interp-WAE (:value 
+                (:assign exp)) env) ;;evaluamos el valor asignado
+            
+            new-env (assoc env id val)];;extendemos el entorno
+        (interp-WAE 
+            
+            (:body exp) new-env));ealuamos el cuerpo con el nuevo entorno
+
+     ;si no es ninguno de loscasos anteriores, lanzamos un error.
+     :else
+     (throw (IllegalArgumentException. "Expresión WAE inválida")))))
--- src/parser.clj
+++ src/parser.clj
@@ -2,11 +2,73 @@
   (:require [grammars :as wae]))
 
 (defn parser-AE
-    "Por implementar"
-    [exp]
-    ())
+  "Convierte una expresión simbólica en una expresión aritmética (AE) de la gramática."
+  [exp]
+  (cond
+    ;;caso 1:Si es un número, lo convertimos a NumG
+    (number? exp)
+    (wae/numG exp)
+
+    ;; Caso 2:si es una operación de suma <+>, la convertimos a AddG
+    (and (list? exp) (= '+ (first exp)))
+    (let [[_ izq der] exp] ;; el primer elemento es '+', los otros dos son las subexpresiones
+      (wae/addG (parser-AE izq) (parser-AE der)))
+
+    ;caso 3:si es una operación de resta <->, la convertimos a SubG
+    (and (list? exp) (= '- (first exp)))
+    (let [[_ izq der] exp] ;;el primer elemento es '-', los otros dos son las subexpresiones
+      (wae/subG 
+        (parser-AE izq) 
+        (parser-AE der)))
+
+    ;si no es ninguno de los casos anteriores, lanzamos un error
+    :else
+    (throw (IllegalArgumentException. "Expresión AE inválida"))))
+
 
 (defn parser-WAE
-    "Por implemntar"
-    [exp]
-    ())
+  "Convierte una expresión simbólica en una expresión WAE de la gramática."
+  [exp]
+  (cond
+    ;; Caso 1: Si es un número, lo convertimos a NumG
+    (number? exp)
+    (wae/numG exp)
+
+    ;; Caso 2: Si es un símbolo, lo convertimos en IdG
+    (symbol? exp)
+    (wae/idG exp)
+
+    ; caso 3: Si es una operacion de suma (+), la convertimos a AddG
+    (and 
+      (list? exp) 
+      (= '+ (first exp))
+    )
+    
+    (let [[_ izq der] exp]
+      (wae/addG 
+        (parser-WAE izq) 
+        (parser-WAE der)
+      )
+    )
+
+    ;;caso 4: si es una operacion de resta (-) la convertimos a SubG
+    (and (list? exp) (= '- (first exp)))
+    (let [[_ izq der] exp]
+      (wae/subG (
+        parser-WAE izq) 
+        (parser-WAE der))
+      )
+
+    ;;caso 5: Si es una expresión 'with', la convertimos en WithG
+    (and (list? exp) (= 'with (first exp)))
+    (let [[_ [id valor] body] exp]
+      (wae/withG (wae/bindings id 
+        (parser-WAE valor)
+      ) 
+        (parser-WAE body)
+      )
+    )
+
+    ;si no es niguno de los casos anterires, lanzamos un error.
+    :else
+    (throw (IllegalArgumentException. "Expresión WAE inválida"))))
